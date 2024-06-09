3be82840a32e8fd17c9a6966379427f99af094dd
paulettemal <136779147+paulettemal@users.noreply.github.com>
2024-06-08 20:18:24 -0500

    Aspecto

--- Observer/src/main/java/com/mycompany/observer/App.java
+++ Observer/src/main/java/com/mycompany/observer/App.java
@@ -2,8 +2,11 @@ package com.mycompany.observer;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
@@ -13,18 +16,31 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+    public void start(Stage primaryStage) {
+        HBox root = new HBox();
+        Scene scene = new Scene(root, 300, 150);
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+        Button btnRed = new Button("Rojo");
+        Button btnGreen = new Button("Verde");
+        Button btnBlue = new Button("Azul");
+
+        btnRed.setOnAction(event -> setBackground(scene, "red"));
+        btnGreen.setOnAction(event -> setBackground(scene, "green"));
+        btnBlue.setOnAction(event -> setBackground(scene, "blue"));
+
+        root.getChildren().addAll(btnRed, btnGreen, btnBlue);
+
+        primaryStage.setTitle("Observer con AspectJ");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void setBackground(Scene scene, String color) {
+        scene.setFill(Color.valueOf(color));
+        System.out.println("Color de fondo cambiado a: " + color);
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
-
 }
